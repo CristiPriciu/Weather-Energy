c82a1d0b3f374ec390356b7b46ef5eddb90882c8
Cristi Priciu <cristipriciu@yahoo.com>
2018-07-04 12:33:44 +0300

    1
    1

---
+++ Energy Wheater 2/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Energy_Wheater_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.btnCalculateIncome = new System.Windows.Forms.Button();
+            this.btnGenerateWeather = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.headerPicture = new System.Windows.Forms.PictureBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.headerPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox9.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox9.Location = new System.Drawing.Point(188, 630);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(402, 13);
+            this.textBox9.TabIndex = 35;
+            this.textBox9.Text = "@ Cristi Priciu 2018";
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(27, 582);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(746, 61);
+            this.richTextBox2.TabIndex = 34;
+            this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox8.Location = new System.Drawing.Point(421, 418);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(50, 13);
+            this.textBox8.TabIndex = 33;
+            this.textBox8.Text = "3 $ /MW";
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(275, 415);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(140, 20);
+            this.textBox7.TabIndex = 32;
+            this.textBox7.Text = "0";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCalculateIncome
+            // 
+            this.btnCalculateIncome.ForeColor = System.Drawing.Color.Black;
+            this.btnCalculateIncome.Location = new System.Drawing.Point(265, 453);
+            this.btnCalculateIncome.Name = "btnCalculateIncome";
+            this.btnCalculateIncome.Size = new System.Drawing.Size(184, 66);
+            this.btnCalculateIncome.TabIndex = 31;
+            this.btnCalculateIncome.Text = "Calculate Income";
+            this.btnCalculateIncome.UseVisualStyleBackColor = true;
+            this.btnCalculateIncome.Click += new System.EventHandler(this.btnCalculateIncome_Click);
+            // 
+            // btnGenerateWeather
+            // 
+            this.btnGenerateWeather.ForeColor = System.Drawing.Color.Black;
+            this.btnGenerateWeather.Location = new System.Drawing.Point(42, 453);
+            this.btnGenerateWeather.Name = "btnGenerateWeather";
+            this.btnGenerateWeather.Size = new System.Drawing.Size(176, 66);
+            this.btnGenerateWeather.TabIndex = 30;
+            this.btnGenerateWeather.Text = "Generate Weather";
+            this.btnGenerateWeather.UseVisualStyleBackColor = true;
+            this.btnGenerateWeather.Click += new System.EventHandler(this.btnGenerateWeather_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Location = new System.Drawing.Point(346, 323);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(140, 13);
+            this.textBox6.TabIndex = 29;
+            this.textBox6.Text = "Wind Power";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Location = new System.Drawing.Point(188, 323);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(140, 13);
+            this.textBox5.TabIndex = 28;
+            this.textBox5.Text = "Solar Power";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(27, 323);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(140, 13);
+            this.textBox4.TabIndex = 27;
+            this.textBox4.Text = "Hydro Power";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(346, 288);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 20);
+            this.textBox3.TabIndex = 26;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(188, 288);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
+            this.textBox2.TabIndex = 25;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 288);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 20);
+            this.textBox1.TabIndex = 24;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.ForeColor = System.Drawing.Color.Black;
+            this.btnQuit.Location = new System.Drawing.Point(575, 534);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(147, 42);
+            this.btnQuit.TabIndex = 23;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(346, 102);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(140, 170);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 21;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(188, 102);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(140, 170);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(27, 102);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // headerPicture
+            // 
+            this.headerPicture.Image = ((System.Drawing.Image)(resources.GetObject("headerPicture.Image")));
+            this.headerPicture.Location = new System.Drawing.Point(27, 11);
+            this.headerPicture.Name = "headerPicture";
+            this.headerPicture.Size = new System.Drawing.Size(770, 74);
+            this.headerPicture.TabIndex = 18;
+            this.headerPicture.TabStop = false;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(503, 102);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(294, 426);
+            this.txtLog.TabIndex = 36;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 655);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.btnCalculateIncome);
+            this.Controls.Add(this.btnGenerateWeather);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.headerPicture);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Weather Generation App";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.headerPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button btnCalculateIncome;
+        private System.Windows.Forms.Button btnGenerateWeather;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox headerPicture;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ Energy Wheater 2/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Energy_Wheater_2
+{
+    public partial class Form1 : Form
+    {
+        static int weatherToday = 0; //Contains the level of rain for the current day
+        public static List<int> weatherEachDay = new List<int>(); //Contains the level or rain for each day
+        static IEnumerable<int> daysList = Enumerable.Range(1, 30);
+        static int mwPrice = 3;
+        static int hydroPower = 0;
+        static int windPower = 0;
+        static int solarPower = 0;
+        static int monthlyIncome = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtLog.Text += "          Welcome to the weather generation App";
+            txtLog.Text += Environment.NewLine;
+            txtLog.Text += Environment.NewLine;
+            txtLog.Text += "Click Generate Weather to create random weather for this month, the click on Calculate Income to see the revenue generated.";
+            txtLog.Text += Environment.NewLine;
+            txtLog.Text += Environment.NewLine;
+        }
+
+        public void ForEachMethod(int weatherToday)
+        {
+            txtLog.Text = string.Empty;
+            txtLog.Text += "          Welcome to the weather generation App";
+            txtLog.Text += Environment.NewLine;
+            txtLog.Text += Environment.NewLine;
+            
+            foreach (int day in daysList)
+            {
+                //Random number from 100 
+                //Random rnd = new Random();
+                //int randomRain = rnd.Next(1, 100);
+                int startValue = 1;
+                int endValue = 100;
+                var random = new Random((int)DateTime.Now.Ticks);
+                var randomRain = random.Next(startValue, endValue + 1);
+
+                if (weatherToday == 0)
+                {
+                    if (randomRain > 30)
+                    {
+                        weatherToday = 1;
+                        weatherEachDay.Add(1);
+                        txtLog.Text += "Rain level on day " + day + " is " + weatherToday;
+                        txtLog.Text += Environment.NewLine;
+                    }
+                    else
+                    {
+                        weatherEachDay.Add(0);
+                        txtLog.Text += "\n" + "Rain level on day " + day + " is 0";
+                        txtLog.Text += Environment.NewLine;
+                    }
+                }
+                else if (weatherToday == 1)
+                {
+                    if (randomRain > 40)
+                    {
+                        weatherToday = 2;
+                        weatherEachDay.Add(2);
+                        txtLog.Text += "Rain level on day " + day + " goes up to level " + weatherToday;
+                        txtLog.Text += Environment.NewLine;
+                    }
+                    else
+                    {
+                        weatherToday = 0;
+                        weatherEachDay.Add(0);
+                        txtLog.Text += "Rain level on day " + day + " is at " + weatherToday;
+                        txtLog.Text += Environment.NewLine;
+                    }
+                }
+                else if (weatherToday == 2)
+                {
+                    if (randomRain > 50)
+                    {
+                        weatherToday = 3;
+                        weatherEachDay.Add(3);
+                        txtLog.Text += "Rain level on day " + day + " moves up to " + weatherToday;
+                        txtLog.Text += Environment.NewLine;
+                    }
+                    else
+                    {
+                        weatherToday = 1;
+                        weatherEachDay.Add(1);
+                        txtLog.Text += "Rain level on day " + day + " goes down to " + weatherToday;
+                        txtLog.Text += Environment.NewLine;
+                    }
+                }
+                else if (weatherToday == 3)
+                {
+                    if (randomRain > 60)
+                    {
+                        weatherToday = 4;
+                        weatherEachDay.Add(4);
+                        txtLog.Text += "Rain level on day " + day + " climbs to the dangerous level " + weatherToday;
+                        txtLog.Text += Environment.NewLine;
+                    }
+                    else
+                    {
+                        weatherToday = 2;
+                        weatherEachDay.Add(2);
+                        txtLog.Text += "Rain level on day " + day + " calms down to level " + weatherToday;
+                        txtLog.Text += Environment.NewLine;
+                    }
+                }
+                else if (weatherToday == 4)
+                {
+                    if (randomRain > 75)
+                    {
+                        weatherToday = 5;
+                        weatherEachDay.Add(5);
+                        txtLog.Text += "Rain level on day " + day + " goes to violent storm level " + weatherToday;
+                        txtLog.Text += Environment.NewLine;
+                    }
+                    else
+                    {
+                        weatherToday = 3;
+                        weatherEachDay.Add(3);
+                        txtLog.Text += "Rain level on day " + day + " is a less dangerous level " + weatherToday;
+                        txtLog.Text += Environment.NewLine;
+                    }
+                }
+                else if (weatherToday == 5)
+                {
+                    weatherToday = 3;
+                    weatherEachDay.Add(5);
+                    txtLog.Text += "Rain level on day " + day + " after the storm, rests at level 3 ";
+                    txtLog.Text += Environment.NewLine;
+                }
+                //txtLog.Text += "Random: " + randomRain + Environment.NewLine; for testing (Random Next issue)
+                System.Threading.Thread.Sleep(3); //pause for 3 miliseconds to eliminate the Random Next issue(duplicate randoms)
+            }
+        }
+        public static void PowerGeneration(List<int> weatherEachDay)
+        {
+            foreach (int z in weatherEachDay)
+            {
+                hydroPower += z;
+                if (z <= 4)
+                {
+                    windPower += z;
+                }
+                else
+                {
+                    windPower += 0;
+                }
+                switch (z)
+                {
+                    case 0:
+                        solarPower += 5;
+                        break;
+                    case 1:
+                        solarPower += 4;
+                        break;
+                    case 2:
+                        solarPower += 3;
+                        break;
+                    case 3:
+                        solarPower += 2;
+                        break;
+                    case 4:
+                        solarPower += 1;
+                        break;
+                    case 5:
+                        solarPower += 0;
+                        break;
+                }
+            }
+        }
+        public static void MonthlyIncomeMethod(int mwprice)
+        {
+            monthlyIncome = (hydroPower + solarPower + windPower) * mwPrice;
+        }
+
+        public void btnGenerateWeather_Click(object sender, EventArgs e)
+        {
+            weatherEachDay.Clear();
+            ForEachMethod(weatherToday);
+        }
+
+        public void btnCalculateIncome_Click(object sender, EventArgs e)
+        {
+            PowerGeneration(weatherEachDay);
+            MonthlyIncomeMethod(mwPrice);
+            textBox1.Text = hydroPower.ToString();
+            textBox2.Text = solarPower.ToString();
+            textBox3.Text = windPower.ToString();
+            textBox7.Text = monthlyIncome.ToString();
+            hydroPower = 0;
+            solarPower = 0;
+            windPower = 0;
+            monthlyIncome = 0;
+            weatherEachDay.Clear();
+        }
+
+        public void btnQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
